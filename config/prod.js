fd850741956b28009c92a48116331369ca646a4b
Justus <justus.soh@hotmail.com>
2021-03-27 04:38:28 +0800

    feat: add prod config

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "port": 8080,
+  "dbEndpoint": "process.env.DATABASE_URL",
+  "ACCESS_TOKEN_SECRET": "access-testing-token",
+  "REFRESH_TOKEN_SECRET": "refresh-testing-token",
+}
