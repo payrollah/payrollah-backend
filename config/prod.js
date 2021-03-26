27c98056d5fd0ab33e75f3487e4c7c96005524ad
Justus <justus.soh@hotmail.com>
2021-03-27 04:46:46 +0800

    fix: whoopsie

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "port": 8080,
-  "dbEndpoint": "process.env.DATABASE_URL",
+  "port": process.env.PORT,
+  "dbEndpoint": process.env.DATABASE_URL,
   "ACCESS_TOKEN_SECRET": "access-testing-token",
   "REFRESH_TOKEN_SECRET": "refresh-testing-token",
 }
